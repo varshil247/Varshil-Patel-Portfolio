cdb7ef0090ea429fa6fc49924471a7e049c05720
varshil247 <varshil247@gmail.com>
2026-03-18 15:12:35 +0000

    Fixed import error

--- src/components/Sections/Projects.tsx
+++ src/components/Sections/Projects.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { ExternalLink } from "lucide-react";
+// import { ExternalLink } from "lucide-react";
 
 // ! ----------------------------------------------------------------------------------------------
 
